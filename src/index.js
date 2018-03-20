a533739295dfc1072df2543f4199818deb8d3d61
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-20 23:25:17 +0700

    Add {setNode}

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Node from './editor/Node';
+import Node from './editor/node';
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 $(function () {
@@ -6,6 +6,6 @@ $(function () {
   const g = svg.append('g').attr('class', 'root');
   const workspace = g.append('g');
 
-  new Node(50, 50, 'Load Image');
-  new Node(200, 200, 'Rotate');
+  new Node(g, workspace).setNode(50, 50, 'Load Image');
+  new Node(g, workspace).setNode(200, 200, 'Rotate');
 });
