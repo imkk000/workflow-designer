f222237f0e61417307bb3497f44aea64dbcecca4
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-24 23:10:34 +0700

    Rewrite {index.js, editor/node.js, app.scss}

--- src/js/editor/node.js
+++ src/js/editor/node.js
@@ -1,90 +1,42 @@
 import * as d3 from 'd3';
 
-export default (workspace, g) =>
+export default (temp, workspace) =>
   class {
-    setNode(x, y, text) {
+    constructor(x, y, text) {
       this.node_data = [{ x: x, y: y }];
 
-      this.node_group = d3
-        .select('.root')
+      this.node_group = workspace
         .append('g')
         .data(this.node_data)
         .attr('class', 'node_group')
         .attr('transform', `translate(${x}, ${y})`);
 
+      this.text = this.node_group
+        .append('text')
+        .attr('class', 'node_label')
+        .attr('dx', '.7em')
+        .attr('dy', '1.5em')
+        .attr('text-anchor', 'start')
+        .style('font', 'bold 1em sans-serif, Verdana, Arial, Helvetica')
+        .text(text);
+
+      const textBBox = this.text.node().getBBox();
+
       this.node = this.node_group
-        .append('rect')
+        .insert('rect', ':first-child')
         .attr('class', 'node')
         .attr('rx', 5)
         .attr('ry', 5)
         .attr('fill', 'lightBlue')
-        .attr('width', 125)
-        .attr('height', 40);
+        .attr('width', textBBox.width + 20)
+        .attr('height', textBBox.height + 20);
 
-      this.line = this.node_group
-        .append('path')
-        .attr('class', 'node_line')
-        .attr('d', 'M 30 1 l 0 38');
-
-      this.text = this.node_group
-        .append('text')
-        .attr('class', 'node_label')
-        .attr('x', 38)
-        .attr('y', 19)
-        .attr('dy', '.35em')
-        .attr('text-anchor', 'start')
-        .text(text);
-
-      this.port_input = this.node_group
-        .append('g')
-        .attr('class', 'port_input')
-        .attr('transform', 'translate(-7, 14)')
-        .append('rect')
-        .attr('class', 'port')
-        .attr('rx', 3)
-        .attr('ry', 3)
-        .attr('width', 14)
-        .attr('height', 14);
-
-      this.port_output = this.node_group
-        .append('g')
-        .attr('class', 'port_output')
-        .attr('transform', `translate(${this.node.attr('width') - 7}, 14)`)
-        .append('rect')
-        .attr('class', 'port')
-        .attr('rx', 3)
-        .attr('ry', 3)
-        .attr('width', 14)
-        .attr('height', 14);
-
-      this.drawable = false;
       this.loadEvent();
     }
 
     loadEvent() {
       this.node_group.call(d3.drag().on('drag', this.handleNodeDrag));
-
-      this.port_input
-        .on('mouseover', this.handlePortMouseOver)
-        .on('mouseout', this.handlePortMouseOut)
-        .call(
-          d3
-            .drag()
-            .on('start', this.handlePortDragStart)
-            .on('drag', this.handlePortDragging)
-            .on('end', this.handlePortDragEnd)
-        );
-
-      this.port_output
-        .on('mouseover', this.handlePortMouseOver)
-        .on('mouseout', this.handlePortMouseOut)
-        .call(
-          d3
-            .drag()
-            .on('start', this.handlePortDragStart)
-            .on('drag', this.handlePortDragging)
-            .on('end', this.handlePortDragEnd)
-        );
+      this.node_group.on('dblclick', this.handleNodeDblClick);
     }
 
     handleNodeDrag(d) {
@@ -94,54 +46,7 @@ export default (workspace, g) =>
       d3.select(this).attr('transform', `translate(${d.x}, ${d.y})`);
     }
 
-    handlePortMouseOver() {
-      d3.select(this).classed('port_hover', true);
-    }
-
-    handlePortMouseOut() {
-      d3.select(this).classed('port_hover', false);
-    }
-
-    handlePortDragStart() {
-      this.drawable = true;
-      const x = d3.event.sourceEvent.x,
-        y = d3.event.sourceEvent.y;
-      this.source = [x, y];
-
-      const link = d3.linkVertical();
-
-      workspace
-        .append('path')
-        .data([
-          {
-            source: [x, y],
-            target: [x, y]
-          }
-        ])
-        .attr('d', link)
-        .attr('fill', 'none')
-        .attr('stroke-width', 3)
-        .attr('stroke', '#ff7f0e');
-    }
-
-    handlePortDragging() {
-      const link = d3.linkHorizontal();
-      const x = d3.event.sourceEvent.x,
-        y = d3.event.sourceEvent.y;
-
-      workspace
-        .select('path')
-        .data([
-          {
-            source: this.source,
-            target: [x, y]
-          }
-        ])
-        .attr('d', link);
-    }
-
-    handlePortDragEnd() {
-      this.drawable = false;
-      workspace.select('path').remove();
+    handleNodeDblClick(d) {
+      console.log('clicked');
     }
   };
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,17 @@
 import * as d3 from 'd3';
 import Node from './editor/node';
 
+// disable right click contextmenu
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 $(function () {
   const svg = d3.select('body').append('svg');
   const g = svg.append('g').attr('class', 'root');
-  const workspace = g.append('g');
-  const NewNode = Node(workspace, g);
-  new NewNode().setNode(200, 200, 'Load Image');
-  new NewNode().setNode(50, 50, 'Rotate');
+  const temp = g.append('g').attr('class', 'temp');
+  const workspace = g.append('g').attr('class', 'workspace');
+
+  const NewNode = Node(temp, workspace);
+
+  const n1 = new NewNode(200, 200, 'Load Image');
+  const n2 = new NewNode(50, 50, 'Rotate');
 });
