71d0e8bfcee4e794b07f40dbfc3b17fc477cedb8
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-28 23:15:07 +0700

    Add handleNodeGroupClick

--- src/js/editor/node/event.js
+++ src/js/editor/node/event.js
@@ -1,4 +1,4 @@
-import { event, select, linkHorizontal } from 'd3'
+import { event, select } from 'd3'
 import Line from '../line'
 
 export const handleNodeGroupDragging = (data, index, nodes) => {
@@ -7,4 +7,8 @@ export const handleNodeGroupDragging = (data, index, nodes) => {
   const node = nodes[index]
   const nodeSelected = select(node)
   nodeSelected.attr('transform', `translate(${data.x}, ${data.y})`)
+}
+
+export const handleNodeGroupClick = (data, index, nodes) => {
+  console.log(data, index, nodes)
 }
