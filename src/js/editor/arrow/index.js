499eb33a513af97304e4d8311af8638d9360e716
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-28 19:18:03 +0700

    Change function to class

--- src/js/editor/arrow/index.js
+++ src/js/editor/arrow/index.js
@@ -1,16 +1,20 @@
 import { getWorkspace } from '../../utility/getCanvas'
 
-export default () => {
-  const workspace = getWorkspace()
-  workspace
-    .append('marker')
-    .attr('id', 'arrow')
-    .attr('viewBox', '0 -5 10 10')
-    .attr('refX', 0)
-    .attr('refY', 0)
-    .attr('markerWidth', 6)
-    .attr('markerHeight', 6)
-    .attr('orient', 'auto')
-    .append('path')
-    .attr('d', 'M0, -5L10, 0L0, 5')
+export default class {
+  constructor() {
+    this.render()
+  }
+
+  render = () => {
+    const workspace = getWorkspace()
+    const marker = workspace
+      .append('marker')
+      .attr('id', 'arrow')
+      .attr('viewBox', '0 -5 10 10')
+      .attr('markerWidth', 6)
+      .attr('markerHeight', 6)
+      .attr('orient', 'auto')
+
+    marker.append('path').attr('d', 'M0, -5L10, 0L0, 5')
+  }
 }
