4a14a0ecb03563e89048e063b43af13f00845101
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-29 14:16:03 +0700

    Add bootstrap js/css to webpack import

--- src/webpack.imports.js
+++ src/webpack.imports.js
@@ -1,6 +1,9 @@
-import '../node_modules/font-awesome/scss/font-awesome.scss'
 import 'jquery-contextmenu'
-import '../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css'
-
-import './scss/app.scss'
+import 'popper.js'
+import 'bootstrap'
 import './js/app.js'
+
+import '../node_modules/font-awesome/scss/font-awesome.scss'
+import '../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './scss/app.scss'
