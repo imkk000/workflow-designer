1720a07410e4a9785845a5084d9eae3e916026e2
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-07 05:17:22 +0700

    Add {scss, js} app to webpack import

--- frontend/src/webpack.import.js
+++ frontend/src/webpack.import.js
@@ -1,7 +1,11 @@
+// NOTE: Import javascript app dependencies
 import 'jquery-contextmenu'
 import 'popper.js'
 import 'bootstrap'
+import './js/app'
 
+// NOTE: Import style(css, scss) app dependencies
 import '../node_modules/font-awesome/scss/font-awesome.scss'
 import '../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './scss/app.scss'
