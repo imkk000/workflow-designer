a543acd8017b1955a51dfa181dcbd7664eb189d6
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-07 05:11:07 +0700

    Add new package to webpack import
    - Add jquery-contextmenu
    - Add popper.js
    - Add bootstrap
    - Add font-awesome.scss
    - Add jquery.contextMenu.css
    - Add bootstrap.css

---
+++ frontend/src/webpack.import.js
@@ -0,0 +1,7 @@
+import 'jquery-contextmenu'
+import 'popper.js'
+import 'bootstrap'
+
+import '../node_modules/font-awesome/scss/font-awesome.scss'
+import '../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
