39489f2df35d869e6dcfb36a67bbc9765e4723cc
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-08 23:50:26 +0700

    Sort import file in webpack import file

--- frontend/src/webpack.import.js
+++ frontend/src/webpack.import.js
@@ -1,12 +1,14 @@
 // NOTE: Import javascript app dependencies
 import 'd3'
-import 'jquery-contextmenu'
-import 'popper.js'
 import 'bootstrap'
+import 'popper.js'
+import 'jquery-ui-bundle'
+import 'jquery-contextmenu'
 import './js/app'
 
 // NOTE: Import style(css, scss) app dependencies
 import '../node_modules/font-awesome/scss/font-awesome.scss'
-import '../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css'
+import '../node_modules/jquery-ui-bundle/jquery-ui.css'
 import './scss/app.scss'
