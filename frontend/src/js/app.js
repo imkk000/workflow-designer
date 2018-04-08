ea85d2513413d6a875fa7e3e0b69b989545b4c6d
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-08 13:31:29 +0700

    Add Node to app

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,3 +1,5 @@
+import Node from './editor/node'
+
 const load = () => {
   // TODO: create new svg, root area
   const svg = d3.select('svg.diagram-drawing')
@@ -6,6 +8,9 @@ const load = () => {
   // TODO: create new temp area, draw area in root area
   root.append('g').attr('class', 'temp-area-group')
   root.append('g').attr('class', 'draw-area-group')
+
+  new Node({ x: 100, y: 100, text: 'Load Image', fill: 'red' })
+  new Node({ x: 200, y: 200, text: 'Rotate', fill: 'green' })
 }
 
 document.addEventListener('contextmenu', (event) => event.preventDefault())
