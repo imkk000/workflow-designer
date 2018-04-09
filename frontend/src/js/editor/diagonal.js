92d87e258e643a15d644701bed8d608e037720da
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-09 08:02:55 +0700

    Create editor/diagonal.js

---
+++ frontend/src/js/editor/diagonal.js
@@ -0,0 +1,17 @@
+export default ({ beginId, beginNode }) => {
+  // NOTE: computed path
+  const {
+    width: widthBeginNode,
+    height: heightBeginNode,
+  } = beginNode.node().getBBox()
+  const halfHeightBeginNode = heightBeginNode / 2
+  const source = window.NODES[beginId]
+
+  return d3
+    .linkHorizontal()
+    .x(({ id, position }) => {
+      if (id === source.id) return position[0] + widthBeginNode
+      return position[0]
+    })
+    .y(({ position }) => position[1] + halfHeightBeginNode)
+}
