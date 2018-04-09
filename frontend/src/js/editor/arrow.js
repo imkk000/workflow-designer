1cc1f58a7227c336e79d63bdd064e685b7b45c01
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-09 11:56:12 +0700

    Fix new arrow path

--- frontend/src/js/editor/arrow.js
+++ frontend/src/js/editor/arrow.js
@@ -2,13 +2,18 @@ import { getDrawArea } from '../utility/getArea'
 
 export default () => {
   getDrawArea()
+    .append('defs')
     .append('marker')
     .attr('id', 'arrow')
+    .attr('markerUnits', 'strokeWidth')
+    .attr('refX', 0)
+    .attr('refY', 0)
     .attr('viewBox', '0 -5 10 10')
     .attr('markerWidth', 6)
     .attr('markerHeight', 6)
     .attr('orient', 'auto')
 
     .append('path')
-    .attr('d', 'M0, -5L10, 0L0, 5')
+    .attr('d', 'M0,-5L10,0L0,5')
+    .style('fill', 'purple')
 }
