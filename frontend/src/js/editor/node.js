d7f8d891a5236612a4c8a301816f19f9638626b8
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-09 08:02:09 +0700

    Add line debug

--- frontend/src/js/editor/node.js
+++ frontend/src/js/editor/node.js
@@ -1,9 +1,14 @@
 import generateId from '../utility/generateId'
-import { getDrawArea, getTempArea } from '../utility/getArea'
-import { isAddLineMode, getPassData } from '../utility/editorMode'
+import { getDrawArea } from '../utility/getArea'
+import EDITOR_MODE, {
+  isAddLineMode,
+  setEditorMode,
+  getPassDataBeforeClear,
+} from '../utility/editorMode'
+import diagonal from './diagonal'
 
 /**
- * TODO: Node life cycle
+ * NOTE: Node life cycle
  * pass options
  * render
  * loadEvent
@@ -14,99 +19,110 @@ export default class {
     this.render(options)
   }
 
-  render = ({ id = generateId(), x, y, label, fill, stroke }) => {
-    const drawArea = getDrawArea()
+render = ({
+  id = generateId(), x, y, label, fill, stroke,
+}) => {
+  const drawArea = getDrawArea()
+
+  this.defautlFill = fill
+  // DEBUG: only
+  window.NODES[id] = {
+    id,
+    position: [x, y],
+    lines: [],
+  }
+
+  const nodeGroup = drawArea
+    .data([{ x, y }])
+    .append('g')
+    .attr('class', 'node')
+    .attr('id', id)
+    .attr('transform', data => `translate(${data.x}, ${data.y})`)
+
+  const nodeLabel = nodeGroup
+    .append('text')
+    .attr('class', 'node-label')
+    .attr('dx', '.4em')
+    .attr('dy', '1.2em')
+    .attr('text-anchor', 'start')
+    .attr('fill', stroke)
+    .text(label.toUpperCase())
+
+  const textBBox = nodeLabel.node().getBBox()
+  const { width: textWidth, height: textHeight } = textBBox
+  const [rectWidthOffset, rectHeightOffset] = [10, 10]
+
+  const nodeBox = nodeGroup
+    .insert('rect', ':first-child')
+    .attr('class', 'node-box')
+    .attr('width', textWidth + rectWidthOffset)
+    .attr('height', textHeight + rectHeightOffset)
+    .attr('fill', fill)
+    .attr('stroke', stroke)
+
+  this.loadEvent({ nodeGroup, nodeLabel, nodeBox })
+}
+
+loadEvent = ({ nodeGroup }) => {
+  nodeGroup
+    .call(d3
+      .drag()
+      .on('drag', this.handleNodeGroupDragging))
+    .on('click', this.handleNodeGroupClick)
+}
+
+handleNodeGroupDragging(data) {
+// NOTE: Calculate coordinate
+  data.x += d3.event.dx
+  data.y += d3.event.dy
+
+  // DEBUG: only
+  const node = d3.select(this)
+  const nodeId = node.attr('id')
+  window.NODES[nodeId].position = [data.x, data.y]
+
+  node.attr('transform', `translate(${data.x}, ${data.y})`)
+}
+
+handleNodeGroupClick() {
+  if (isAddLineMode()) {
+    // TODO: validate node !!!
+
+    // NOTE: prepare data for add line
+    const { beginId, node: beginNode, defaultStroke } = getPassDataBeforeClear()
+    const endNode = d3.select(this)
+    const endId = endNode.attr('id')
 
     // DEBUG: only
-    window.NODES[id] = {
-      id,
-      position: [x, y],
-      lines: [],
-    }
+    const link = diagonal({ beginId, beginNode })
+    const lineId = generateId()
+    const source = window.NODES[beginId]
+    const target = window.NODES[endId]
 
-    const nodeGroup = drawArea
-      .data([{ x, y }])
-      .append('g')
-      .attr('class', 'node')
-      .attr('id', id)
-      .attr('transform', (data) => `translate(${data.x}, ${data.y})`)
+    // NOTE: add this line to begin and end node.lines
+    source.lines.push(lineId)
+    target.lines.push(lineId)
 
-    const nodeLabel = nodeGroup
-      .append('text')
-      .attr('class', 'node-label')
-      .attr('dx', '.4em')
-      .attr('dy', '1.2em')
-      .attr('text-anchor', 'start')
-      .attr('fill', stroke)
-      .text(label.toUpperCase())
+    // NOTE: add line to global LINES
+    // addDataToGlobal()
+    // NOTE: render line to draw-area-group
+    getDrawArea()
+      .insert('g', ':first-child')
+      .attr('class', 'line')
+      .attr('id', lineId)
+      .data([{ source, target }])
+      .append('path')
+      .attr('d', link)
 
-    const textBBox = nodeLabel.node().getBBox()
-    const { width: textWidth, height: textHeight } = textBBox
-    const [rectWidthOffset, rectHeightOffset] = [10, 10]
+    // NOTE: reset stroke and context menu
+    beginNode.select('.node-box').attr('stroke', defaultStroke)
+    $('g.node').contextMenu(true)
 
-    const nodeBox = nodeGroup
-      .insert('rect', ':first-child')
-      .attr('class', 'node-box')
-      .attr('width', textWidth + rectWidthOffset)
-      .attr('height', textHeight + rectHeightOffset)
-      .attr('fill', fill)
-      .attr('stroke', stroke)
-
-    this.loadEvent({ nodeGroup, nodeLabel, nodeBox })
-  }
-
-  loadEvent = ({ nodeGroup }) => {
-    nodeGroup
-      .call(d3.drag().on('drag', this.handleNodeGroupDragging))
-      .on('click', this.handleNodeGroupClick)
-  }
-
-  handleNodeGroupDragging(data) {
-    // TODO: Calculate coordinate
-    data.x += d3.event.dx
-    data.y += d3.event.dy
+    // NOTE: reset mode to NORMAL
+    setEditorMode(EDITOR_MODE.NORMAL)
 
     // DEBUG: only
-    const node = d3.select(this)
-    const nodeId = node.attr('id')
-    window.NODES[nodeId].position = [data.x, data.y]
-
-    node.attr('transform', `translate(${data.x}, ${data.y})`)
+    console.log(source, target)
   }
-
-  handleNodeGroupClick() {
-    if (isAddLineMode()) {
-      // TODO: calculate beginNode size
-      const { beginId, node: beginNode } = getPassData()
-      const {
-        width: widthBeginNode,
-        height: heightBeginNode,
-      } = beginNode.node().getBBox()
-      const halfHeightBeginNode = heightBeginNode / 2
-      const endNode = d3.select(this)
-      const endId = endNode.attr('id')
-
-      // DEBUG: only
-      const source = window.NODES[beginId]
-      const target = window.NODES[endId]
-
-      const link = d3
-        .linkHorizontal()
-        .x(({ id, position }) => {
-          if (id === source.id) return position[0] + widthBeginNode
-          return position[0]
-        })
-        .y(({ position }) => position[1] + halfHeightBeginNode)
-
-      getTempArea()
-        .data([{ source, target }])
-        .append('path')
-        .attr('class', 'line')
-        .attr('d', link)
-        .style('fill', 'none')
-        .style('stroke-width', 3)
-        .style('stroke', 'blue')
-      // .attr('marker-end', 'url(#arrow)')
-    }
-  }
+}
 }
