e82248a1705e80fe89524ff6dcb93665f4333372
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-09 11:57:32 +0700

    Create editor/updateLine.js

---
+++ frontend/src/js/editor/updateLine.js
@@ -0,0 +1,36 @@
+import { getDataFromGlobal } from '../utility/editorMode'
+import diagonal from './diagonal'
+
+export default ({ node, data }) => {
+  // NOTE: get (nodes, lines) data from global
+  const nodes = getDataFromGlobal('NODES')
+  const lines = getDataFromGlobal('LINES')
+
+  // NOTE: get node id (this node from event)
+  const nodeId = node.attr('id')
+  const { [nodeId]: { lines: linesData } } = nodes
+
+  /* FIXME: incorrect way to access data
+            fix later (setGlobal :: utility/editorNode.js) */
+  const nodeData = nodes[nodeId]
+  nodeData.position = [data.x, data.y]
+
+  // NOTE: update line when node moving
+  linesData.map((lineId) => {
+    const { [lineId]: { beginId, endId } } = lines
+    const beginNode = d3.select($(`#${beginId}`).get(0))
+    const line = d3.select($(`#${lineId} path`).get(0))
+    const source = (nodeId === beginId) ? nodeData : nodes[beginId]
+    const target = (nodeId === endId) ? nodeData : nodes[endId]
+    const link = diagonal({ beginId, beginNode })
+
+    line
+      .data([{
+        source,
+        target,
+      }])
+      .attr('d', link)
+
+    return true
+  })
+}
