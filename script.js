fe3e9a2868f517d7d177398fc543dd3be4501025
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-12 02:57:43 +0700

    Update code

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
 source = {
-  x: 300,
+  x: 100,
   y: 100
 };
 target = {
   x: 100,
-  y: 100
+  y: 300
+};
+arrow = {
+  width: 6,
+  height: 6
 };
 middle = {
   x: (source.x + target.x) / 2,
   y: (source.y + target.y) / 2
 };
+
+// set line instead of original
+(function() {
+  rad = Math.atan2(target.y - source.y, target.x - source.x); // rad
+  r = arrow.width * 3; // arrow size
+  end = {
+    x: r * Math.cos(rad) + target.x,
+    y: r * Math.sin(rad) + target.y
+  };
+
+  line = document.querySelector('#line-new');
+  _ =
+    `M ${source.x} ${source.y},` +
+    `C ${middle.x} ${source.y},` +
+    `${middle.x} ${target.y},` +
+    `${target.x} ${target.y},`;
+
+  console.log('_new_:', _);
+  line.setAttribute('d', _);
+
+  arrow = document.querySelector('#arrow');
+  arrow.setAttribute('refX', 0);
+  arrow.setAttribute('refY', 3);
+
+  path = arrow.querySelector('path');
+  _ = `M 0 0,  L 0 6, 6 3 z`;
+  path.setAttribute('d', _);
+})();
 
 // set circle
 (function() {
@@ -24,23 +56,12 @@ middle = {
 
 // set line original
 (function() {
-  lineGuide = document.querySelector('#line-guide');
-  _ =
-    `M ${source.x},${source.y} ` +
-    `C ${middle.x},${source.y} ` +
-    `${middle.x},${target.y} ` +
-    `${target.x},${target.y} `;
-  console.log('_ori_:', _);
-  lineGuide.setAttribute('d', _);
-})();
-
-// set line instead of original
-(function() {
-  line = document.querySelector('#line-new');
-  _ =
-    `M ${source.x},${source.y} ` +
-    `Q ${middle.x},${source.y} ${middle.x},${middle.y} ` +
-    `Q ${middle.x},${target.y} ${target.x},${target.y} `;
-  console.log('_new_:', _);
-  line.setAttribute('d', _);
+  // lineGuide = document.querySelector('#line-guide');
+  // _ =
+  //   `M ${source.x} ${source.y},` +
+  //   `C ${middle.x} ${source.y},` +
+  //   `${middle.x} ${target.y},` +
+  //   `${target.x} ${target.y},`;
+  // console.log('_ori_:', _);
+  // lineGuide.setAttribute('d', _);
 })();
