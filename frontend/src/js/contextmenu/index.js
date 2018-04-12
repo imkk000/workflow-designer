95e13876c40e8e9b3e3e09ce43b7d67c26c1ff45
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-13 05:20:39 +0700

    Create contextmenu/index.js for context menu load on page ready

---
+++ frontend/src/js/contextmenu/index.js
@@ -0,0 +1,8 @@
+import nodeContextMenu from './nodeContextMenu'
+import drawAreaContextMenu from './drawAreaContextMenu'
+
+// NOTE: load all context menu
+$(document).ready(() => {
+  nodeContextMenu()
+  drawAreaContextMenu()
+})
