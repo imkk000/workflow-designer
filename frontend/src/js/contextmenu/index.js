d704d0d382087885b05c79c852ec8b1749b19709
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-13 06:23:11 +0700

    Remove drawAreaContextMenu from contextmenu load

--- frontend/src/js/contextmenu/index.js
+++ frontend/src/js/contextmenu/index.js
@@ -1,8 +1,6 @@
 import nodeContextMenu from './nodeContextMenu'
-import drawAreaContextMenu from './drawAreaContextMenu'
 
 // NOTE: load all context menu
 $(document).ready(() => {
   nodeContextMenu()
-  drawAreaContextMenu()
 })
