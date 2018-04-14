f81ade5d9df7cf1e6f86c0459140fcba0d008562
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-14 18:58:02 +0700

    Enable eslint in webpack config file

--- frontend/webpack.config.babel.js
+++ frontend/webpack.config.babel.js
@@ -26,12 +26,12 @@ const prodConfig = {
   ],
   module: {
     rules: [
-      // {
-      //   enforce: 'pre',
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   loader: ['babel-loader', 'eslint-loader'],
-      // },
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: ['babel-loader', 'eslint-loader'],
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
