7d7f5d03bf9c8937c7f935db409675d75356b5ae
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-14 21:35:02 +0700

    Add setting node content

--- frontend/src/js/mode/settingNodeMode.jsx
+++ frontend/src/js/mode/settingNodeMode.jsx
@@ -1,17 +1,26 @@
 import dom from 'jsx-render'
 import { showDialog } from '../editor/dialog'
+import { getPassDataBeforeClear } from '../utility/editorMode'
 
 export default () => {
+  const { nodeId, node } = getPassDataBeforeClear()
+
   const settingType = {
     custom: true,
     id: 'setting-dialog',
-    title: 'Settings?',
+    title: `Node Setting: id = ${nodeId}`,
     button: [],
   }
 
   const content = (
     <div id={settingType.id}>
-      <p>Setting?</p>
+      <form>
+        <fieldset>
+          <label for="name">Upload Image:</label>
+          <input type="file" name="upload-file" class="upload-file ui-widget-content ui-corner-all" />
+          {/* <input type="submit" tabindex="-1" style="position:absolute; top:-1000px" /> */}
+        </fieldset>
+      </form>
     </div>
   )
   showDialog(settingType, content)
