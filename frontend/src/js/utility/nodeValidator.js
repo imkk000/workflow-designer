4af2b12442a5b5425f0a3afd94231058741bef5f
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-14 08:25:55 +0700

    Add isDuplicate to node validator and remove unused variable

--- frontend/src/js/utility/nodeValidator.js
+++ frontend/src/js/utility/nodeValidator.js
@@ -2,12 +2,13 @@ import { errorDialog } from '../editor/dialog'
 import { getDataFromGlobal } from './editorMode'
 
 let nodes
-let lines
 let graph
 
 const isSelfNode = ({ beginId, endId }) => beginId === endId
 
 const isLimitInput = ({ endId }) => nodes[endId].limitInput === graph.indegree(endId)
+
+const isDuplicate = ({ beginId, endId }) => graph.adjacent(beginId).filter(nodeId => nodeId === endId).length
 
 const isUndirected = ({ beginId, endId }) => graph.adjacent(endId).filter(nodeId => nodeId === beginId).length
 
@@ -19,10 +20,9 @@ const showErrorDialog = () => {
 export default nodeData => {
   // NOTE: first, get all data from global
   nodes = getDataFromGlobal('NODES')
-  lines = getDataFromGlobal('LINES')
   graph = getDataFromGlobal('GRAPH')
 
-  const validateFunctionList = [isSelfNode, isLimitInput, isUndirected]
+  const validateFunctionList = [isSelfNode, isLimitInput, isDuplicate, isUndirected]
 
   for (let i = 0; i < validateFunctionList.length; i += 1)
     if (validateFunctionList[i](nodeData)) return showErrorDialog()
