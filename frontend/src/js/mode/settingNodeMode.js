d386bf45390bf3e791c2e8b85af9e8820b71d6bb
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-15 06:43:49 +0700

    Fix upload file setting for load_image

--- frontend/src/js/mode/settingNodeMode.js
+++ frontend/src/js/mode/settingNodeMode.js
@@ -1,5 +1,10 @@
 import uploadFileDialog from './settingDialog/upload-file-dialog'
+import { getDataFromGlobal, getPassData } from '../utility/editorMode'
 
 export default () => {
-  uploadFileDialog()
+  const nodes = getDataFromGlobal('NODES')
+  const { nodeId } = getPassData()
+  const { type } = nodes[nodeId]
+
+  if (type === 'load_image') uploadFileDialog()
 }
