2f6849a80564c812b4cd4b924f9dd0d90e63067c
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-15 06:17:41 +0700

    Add form for upload file

--- frontend/src/js/mode/settingDialog/upload-file-dialog.jsx
+++ frontend/src/js/mode/settingDialog/upload-file-dialog.jsx
@@ -1,53 +1,105 @@
 import dom from 'jsx-render'
-import { showDialog, errorDialog } from '../../editor/dialog'
-import { getPassDataBeforeClear } from '../../utility/editorMode'
+import { isEmpty } from 'validator'
+import { showDialog, errorDialog, confirmDialog } from '../../editor/dialog'
+import { getPassDataBeforeClear, getDataFromGlobal } from '../../utility/editorMode'
+
+let uploadId = null
+let nodes = null
+
+const startUploadFile = () => {
+  if (uploadId) {
+    $('input[name=file]').dmUploader('start', uploadId)
+  }
+}
 
 export default () => {
-  const { nodeId, node } = getPassDataBeforeClear()
+  nodes = getDataFromGlobal('NODES')
 
-  const settingType = {
-    custom: true,
-    id: 'setting-dialog',
-    title: `Node Setting: id = ${nodeId}`,
-    buttons: [
-      {
-        text: 'Cancel',
-        click() {
-          $(this).dialog('close')
-        },
-      },
-    ],
+  const { nodeId } = getPassDataBeforeClear()
+  const {
+    files: { fileId, fileName },
+  } = nodes[nodeId]
+  const existingFile = !isEmpty(fileId)
+  const initialFileName = existingFile ? fileName : 'No image uploaded...'
+
+  const dialogId = 'setting-dialog'
+  const title = `Setting Node id ${nodeId}`
+  const buttons = {
+    OK() {
+      if (uploadId) {
+        const content = `Do you want to ${existingFile ? 'replace existing file?' : 'continue?'}`
+        confirmDialog(content, () => {
+          startUploadFile()
+          $('#confirm-dialog').dialog('close')
+        })
+      } else {
+        errorDialog('Please select image for upload')
+      }
+    },
+    Cancel() {
+      $(this).dialog('close')
+    },
   }
-
   const content = (
-    <div id={settingType.id}>
-      <form method="post">
+    <div id={dialogId}>
+      <form>
         <fieldset>
-          <label for="upload-file">Upload Image:</label>
-          <input type="file" name="upload-file" class="upload-file ui-widget-content ui-corner-all" />
+          <label for="file">Upload Image:</label>
+          <input
+            type="text"
+            name="file-information"
+            class="text ui-widget-content ui-corner-all"
+            placeholder={initialFileName}
+            disabled
+          />
+          <div class="file ui-widget-content ui-corner-all">
+            Open the file Browser
+            <input type="file" name="file" id="file" title="Open the file Browser" />
+          </div>
         </fieldset>
       </form>
     </div>
   )
-  showDialog(settingType, content)
-  $('input[name=upload-file]').dmUploader({
+
+  showDialog({
+    custom: true,
+    id: dialogId,
+    title,
+    buttons,
+    content,
+  })
+
+  $('input[name=file]').dmUploader({
     // DEBUG: url upload file server
-    url: '//127.0.0.1:9999/upload',
-    // auto: false,
+    url: '//0.0.0.0:9999/upload',
+    auto: false,
     multiple: false,
-    // NOTE: file size limit 10 mib
     maxFileSize: 10485760,
     allowedTypes: 'image/*',
     extFilter: ['jpg', 'jpeg', 'png'],
-    onInit() {
-      console.log('Callback: Plugin initialized')
+    onNewFile(id, file) {
+      $('input[name=file-information]').val(file.name)
+      uploadId = id
     },
-    onFileTypeError(id, xhr, status, errorThrown) {
-      console.log(status, errorThrown)
-      errorDialog(errorThrown)
+    onFileTypeError() {
+      errorDialog('File support only image')
+    },
+    onFileSizeError() {
+      errorDialog('Max file size limit 10 MiB')
+    },
+    onFileExtError() {
+      errorDialog('File support [ jpg, jpeg, png ]')
+    },
+    onUploadError() {
+      errorDialog('File upload error, please try again')
     },
     onUploadSuccess(id, data) {
       console.log(data)
+      nodes[nodeId].files = data
+
+      // NOTE: destroy uploader and close dialog
+      $(this).dmUploader('destroy')
+      $(`#${dialogId}`).dialog('close')
     },
   })
 }
