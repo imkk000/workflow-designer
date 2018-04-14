87919edf5d1dfc4ad516455803516f4cb918f0f5
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-14 07:37:59 +0700

    Add isLimitInput endId

--- frontend/src/js/utility/nodeValidator.js
+++ frontend/src/js/utility/nodeValidator.js
@@ -1,15 +1,23 @@
 import { errorDialog } from '../editor/dialog'
+import { getDataFromGlobal } from './editorMode'
+
+const nodes = getDataFromGlobal('NODES')
+const lines = getDataFromGlobal('LINES')
+const graph = getDataFromGlobal('GRAPH')
 
 const isSelfNode = ({ beginId, endId }) => beginId === endId
+
+const isLimitInput = ({ endId }) => nodes[endId].limitInput === graph.indegree(endId)
 
 const showErrorDialog = () => {
   errorDialog('You can not select this node, please try again.')
   return true
 }
 
-export default nodes => {
-  const result = isSelfNode(nodes)
-  if (result) return showErrorDialog()
+export default nodeData => {
+  console.log(nodeData[nodeData.beginId].limitInput === graph.indegree(nodeData.endId))
+  if (isSelfNode(nodeData)) return showErrorDialog()
+  if (isLimitInput(nodeData)) return showErrorDialog()
 
   // NOTE: this connection is ok
   return false
