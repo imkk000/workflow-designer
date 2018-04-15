caad676495f668ab74928076bd7f76ec75d8e50c
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-15 09:48:19 +0700

    Add checkOk and edit when click Ok button

--- frontend/src/js/mode/settingDialog/set-parameter-dialog.jsx
+++ frontend/src/js/mode/settingDialog/set-parameter-dialog.jsx
@@ -1,10 +1,14 @@
 import dom from 'jsx-render'
-import { showDialog, errorDialog } from '../../editor/dialog'
+import { showDialog, confirmDialog } from '../../editor/dialog'
 import EDITOR_MODE, { setEditorMode, getDataFromGlobal, getPassDataBeforeClear } from '../../utility/editorMode'
 
+let settingsKeysToArray = null
+let settingsValuesToArray = null
 let nodes = null
 
 const quitSettingNodeMode = () => {
+  settingsKeysToArray = null
+  settingsValuesToArray = null
   setEditorMode(EDITOR_MODE.NORMAL)
 }
 
@@ -13,10 +17,68 @@ export default validator => {
 
   const { nodeId } = getPassDataBeforeClear()
   const { settings } = nodes[nodeId]
+
   const dialogId = 'setting-dialog'
   const title = `Setting Node id ${nodeId}`
+  const checkOk = () => {
+    const valid = settingsKeysToArray.map(key => {
+      const { validator: validateInput } = settings[key]
+      const valueString = $(`input[name=${key}]`).val()
+      const value = parseInt(valueString, 10)
+      return validateInput(value)
+    })
+    const errorDialogContent = valid.map((validResult, index) => {
+      if (!validResult) {
+        const key = settingsKeysToArray[index]
+        const { value, errorText } = settingsValuesToArray[index]
+        $(`input[name=${key}]`).val(value)
+
+        return <p>- {errorText}</p>
+      }
+      return false
+    })
+    const validCount = errorDialogContent.reduce((sum, current) => sum + (current !== false))
+
+    if (validCount) {
+      const id = 'error-dialog'
+      const buttons = {
+        Close() {
+          $(this).dialog('close')
+        },
+      }
+      const content = <div id={id}>{errorDialogContent}</div>
+
+      showDialog({
+        custom: true,
+        id,
+        title: 'Error Dialog',
+        buttons,
+        content,
+      })
+      return false
+    }
+
+    return true
+  }
+
   const buttons = {
-    OK() {},
+    OK() {
+      if (checkOk()) {
+        confirmDialog('Do you want to continue?', () => {
+          // NOTE: set values
+          settingsValuesToArray.forEach((setting, index) => {
+            const key = settingsKeysToArray[index]
+            const valueString = $(`input[name=${key}]`).val()
+            const value = parseInt(valueString, 10)
+            setting.value = value
+          })
+
+          quitSettingNodeMode()
+          $('#confirm-dialog').dialog('close')
+          $(this).dialog('close')
+        })
+      }
+    },
     Cancel() {
       quitSettingNodeMode()
       $(this).dialog('close')
@@ -48,6 +110,7 @@ export default validator => {
       </form>
     </div>
   )
+
   showDialog({
     custom: true,
     id: dialogId,
@@ -55,4 +118,7 @@ export default validator => {
     buttons,
     content,
   })
+
+  settingsKeysToArray = Object.keys(settings)
+  settingsValuesToArray = Object.values(settings)
 }
