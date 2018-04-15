34b56349d53ff2c9152eaea49c0c77d075a4470b
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-16 04:22:56 +0700

    Create utility/attachDragAndDrop.js

---
+++ frontend/src/js/utility/attachDragAndDrop.js
@@ -0,0 +1,37 @@
+import Node from '../editor/node'
+import { getDataFromGlobal } from '../utility/editorMode'
+
+const allowDrop = event => {
+  event.preventDefault()
+}
+
+const drop = event => {
+  event.preventDefault()
+
+  if (event.target.id === 'diagram-drawing') {
+    const buffer = getDataFromGlobal('NODES_BUFFER')
+    const data = event.dataTransfer.getData('text')
+    const nodeBuffer = buffer[data]
+    const nodeData = {
+      ...nodeBuffer,
+      x: event.offsetX,
+      y: event.offsetY,
+    }
+
+    // NOTE: create new Node
+    new Node(nodeData)
+  }
+}
+
+const drag = event => {
+  event.dataTransfer.setData('text', event.target.id)
+}
+
+export default (svg, node) => {
+  const svgDom = svg.node()
+  const nodeDom = node.node()
+
+  nodeDom.addEventListener('dragstart', drag)
+  svgDom.addEventListener('drop', drop)
+  svgDom.addEventListener('dragover', allowDrop)
+}
