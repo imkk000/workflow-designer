c3ba0f5a394c34b04c0311b66cfda5d55af06cdf
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-16 05:45:31 +0700

    Change app config file to app.config.js

--- app.config.js
+++ app.config.js
@@ -1,8 +1,6 @@
-const ABOUT_APP = {
+module.exports = {
   name: 'workflow-designer',
   title: 'Workflow Designer for Basic Image Processing',
   version: '0.0.1',
   author: 'Nattakit Boonyang (5635512005)',
 }
-
-export default ABOUT_APP
---
+++ frontend/app.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'workflow-designer',
+  title: 'Workflow Designer for Basic Image Processing',
+  version: '0.0.1',
+  author: 'Nattakit Boonyang (5635512005)',
+}
