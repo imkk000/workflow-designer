95c7010d21ed6150c502f99654b5d6dcde6952d6
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-16 17:36:45 +0700

    Import mock start process

--- frontend/src/webpack.import.js
+++ frontend/src/webpack.import.js
@@ -11,6 +11,7 @@ import './js/app'
 import './js/onload/addNodePreviews'
 import './js/contextmenu'
 import './js/editor/grid'
+import './js/onload/mock'
 
 // NOTE: Import style(css, scss) app dependencies
 import '../node_modules/roboto-fontface/css/roboto/sass/roboto-fontface.scss'
