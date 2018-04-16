6bcf9e5f76c456069e5e052954d42816d8984093
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-17 05:54:44 +0700

    Add queue to mock request for process

--- frontend/src/js/onload/mock.js
+++ frontend/src/js/onload/mock.js
@@ -1,7 +1,26 @@
 import axios from 'axios'
+import { getDataFromGlobal } from '../utility/editorMode'
+import { informationDialog } from '../editor/dialog'
 
 window.addEventListener('load', () => {
   document.getElementById('start-process').addEventListener('click', () => {
-    axios.post('http://127.0.0.1:9999/api/process', {})
+    const nodes = getDataFromGlobal('NODES')
+    const graph = getDataFromGlobal('GRAPH')
+    const topologicalSort = graph.topologicalSort()
+    const queue = topologicalSort
+    const queueNext = () => {
+      const nodeId = queue.shift()
+      if (nodeId) {
+        axios.post('http://127.0.0.1:9999/api/process', nodes[nodeId]).then(({ data }) => {
+          if (queue.length) {
+            nodes[queue[0]].files = data
+            queueNext()
+          } else {
+            informationDialog('Process Complete')
+          }
+        })
+      }
+    }
+    queueNext()
   })
 })
