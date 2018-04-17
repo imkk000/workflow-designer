6c4a2ae6713e716f39abcefabde759712d002263
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-18 02:13:56 +0700

    Fix range value for validate value

--- frontend/src/js/functions/nodeProperties/RotateFunction.js
+++ frontend/src/js/functions/nodeProperties/RotateFunction.js
@@ -9,15 +9,15 @@ export default {
   settings: {
     angle: {
       defaultValue: 90,
-      validatorOptions: { gt: -360, lt: 360 },
+      validatorOptions: { min: -359, max: 359 },
       get validator() {
         return value => isInt(value, this.validatorOptions)
       },
       get label() {
-        return `angle (${this.validatorOptions.gt}- ${this.validatorOptions.lt}) [degree]`
+        return `angle (${this.validatorOptions.min}- ${this.validatorOptions.max}) [degree]`
       },
       get errorText() {
-        return `angle has value between ${this.validatorOptions.gt} degree to ${this.validatorOptions.lt} degree`
+        return `angle has value between ${this.validatorOptions.min} degree to ${this.validatorOptions.max} degree`
       },
     },
   },
