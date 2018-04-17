f2464a8a361c658eafe3753c37b544afaed4679b
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-18 03:16:00 +0700

    Add quitProcessMode

--- frontend/src/js/mode/processMode.js
+++ frontend/src/js/mode/processMode.js
@@ -3,18 +3,38 @@ import { isLength } from 'validator'
 import EDITOR_MODE, { getDataFromGlobal, notNormalMode, setEditorMode } from '../utility/editorMode'
 import { informationDialog, errorDialog, confirmDialog } from '../editor/dialog'
 
+let startProcessState = false
+
 const quitProcessMode = () => {
+  startProcessState = true
+  toggleMode()
   setEditorMode(EDITOR_MODE.NORMAL)
 }
 
+const toggleMode = () => {
+  if (startProcessState) {
+    document.getElementById('process').innerText = 'Start Process'
+    startProcessState = false
+  } else {
+    document.getElementById('process').innerText = 'Stop Process'
+    startProcessState = true
+  }
+}
+
 window.addEventListener('load', () => {
-  document.getElementById('start-process').addEventListener('click', () => {
+  document.getElementById('process').addEventListener('click', () => {
     if (notNormalMode()) {
       errorDialog('Process can use only NORMAL mode')
       return
     }
 
-    setEditorMode(EDITOR_MODE.NORMAL)
+    if (startProcessState) {
+      informationDialog('Exit process mode')
+      quitProcessMode()
+      return
+    }
+
+    setEditorMode(EDITOR_MODE.PROCESS)
 
     const nodes = getDataFromGlobal('NODES')
     const graph = getDataFromGlobal('GRAPH')
@@ -24,12 +44,17 @@ window.addEventListener('load', () => {
       .filter(nodeId => nodes[nodeId].lines.length > 0)
       .map(nodeId => nodes[nodeId])
 
-    if (topologicalSort.length === 0) return
+    if (topologicalSort.length === 0) {
+      errorDialog('Functions not found, please try again')
+      quitProcessMode()
+      return
+    }
 
     // check empty file
     const loadImageFunctions = topologicalSort.filter(({ type }) => type === 'LoadImageFunction')
     if (loadImageFunctions.length === 0) {
       errorDialog('LoadImageFunction not found')
+      quitProcessMode()
       return
     }
 
@@ -39,52 +64,69 @@ window.addEventListener('load', () => {
     }, true)
 
     if (!isFilesValid) {
-      errorDialog("LoadImageFunction don't have image file")
+      errorDialog('LoadImageFunction does not have image file')
+      quitProcessMode()
       return
     }
 
     // start process
     const queue = topologicalSort
     const queueNext = () => {
-      const node = queue.shift()
-      if (node) {
-        const { id, type, files, settings } = node
-        // deep clone
-        const newSettings = JSON.parse(JSON.stringify(settings))
-        Object.keys(newSettings).map(key => {
-          const { value } = newSettings[key]
-          newSettings[key] = value
-          return true
-        })
+      if (startProcessState) {
+        const node = queue.shift()
+        if (node) {
+          const { id, type, files, settings } = node
+          // deep clone
+          const newSettings = JSON.parse(JSON.stringify(settings))
+          Object.keys(newSettings).map(key => {
+            const { value } = newSettings[key]
+            newSettings[key] = value
+            return true
+          })
 
-        const sendData = {
-          id,
-          type,
-          files,
-          settings: newSettings,
+          const sendData = {
+            id,
+            type,
+            files,
+            settings: newSettings,
+          }
+
+          axios
+            .post('http://127.0.0.1:9999/api/process', sendData)
+            .then(({ data }) => {
+              if (!startProcessState) return
+
+              if (queue.length) {
+                if (data.status) {
+                  errorDialog('something error on server, please try again')
+                  return
+                }
+
+                queue[0].files = data
+                queueNext()
+              } else {
+                informationDialog('Process Complete')
+                quitProcessMode()
+              }
+            })
+            .catch(error => {
+              errorDialog(error)
+              quitProcessMode()
+            })
         }
-
-        axios
-          .post('http://127.0.0.1:9999/api/process', sendData)
-          .then(({ data }) => {
-            if (queue.length) {
-              queue[0].files = data
-              queueNext()
-            } else {
-              informationDialog('Process Complete')
-              setEditorMode(EDITOR_MODE.NORMAL)
-            }
-          })
-          .catch(error => {
-            errorDialog(error)
-            setEditorMode(EDITOR_MODE.NORMAL)
-          })
       }
     }
 
-    confirmDialog('Do you want to continue?', () => {
+    // add custom callback
+    const okCallback = () => {
+      toggleMode()
       queueNext()
       $('#confirm-dialog').dialog('close')
-    })
+    }
+    const cancelCallback = () => {
+      quitProcessMode()
+      $('#confirm-dialog').dialog('close')
+    }
+    confirmDialog('Do you want to continue?', okCallback, cancelCallback)
   })
 })
