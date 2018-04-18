ec285bdfe8d68d350693d96aede40c66ececde0b
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-18 07:19:48 +0700

    Remove {bootstrap, popper.js, mode/processMode} from webpack import file

--- frontend/src/webpack.import.js
+++ frontend/src/webpack.import.js
@@ -1,7 +1,5 @@
 // NOTE: Import javascript app dependencies
 import 'd3'
-import 'bootstrap'
-import 'popper.js'
 import 'jquery-ui-bundle'
 import 'jquery-contextmenu'
 import 'dm-file-uploader'
@@ -11,12 +9,10 @@ import './js/app'
 import './js/onload/addNodePreviews'
 import './js/contextmenu'
 import './js/editor/grid'
-import './js/mode/processMode'
 
 // NOTE: Import style(css, scss) app dependencies
 import '../node_modules/roboto-fontface/css/roboto/sass/roboto-fontface.scss'
 import '../node_modules/font-awesome/scss/font-awesome.scss'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css'
 import '../node_modules/jquery-ui-bundle/jquery-ui.css'
 import './scss/app.scss'
