82ec671bcd59b47628836cbc10e48aa56f2aa301
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-22 16:18:39 +0700

    Remove comment

--- frontend/src/js/utility/editorMode.js
+++ frontend/src/js/utility/editorMode.js
@@ -1,6 +1,6 @@
 import showCurrentMode from '../editor/showCurrentMode'
 
-// NOTE: initial constant editor mode
+// initial constant editor mode
 const EDITOR_MODE = {
   NORMAL: 'NORMAL',
   ADD_LINE: 'ADD_LINE',
@@ -11,65 +11,65 @@ const EDITOR_MODE = {
   PROCESS: 'PROCESS',
 }
 
-// NOTE: check mode is NORMAL
+// check mode is NORMAL
 export const isNormalMode = () => window.EDITOR_MODE === EDITOR_MODE.NORMAL
 
-// NOTE: check mode is not NORMAL
+// check mode is not NORMAL
 export const notNormalMode = () => !isNormalMode()
 
-// NOTE: check mode is ADD_LINE
+// check mode is ADD_LINE
 export const isAddLineMode = () => window.EDITOR_MODE === EDITOR_MODE.ADD_LINE
 
-// NOTE: check mode is SETTING
+// check mode is SETTING
 export const isSettingMode = () => window.EDITOR_MODE === EDITOR_MODE.SETTING
 
-// NOTE: check mode is ERROR
+// check mode is ERROR
 export const isErrorMode = () => window.EDITOR_MODE === EDITOR_MODE.ERROR
 
-// NOTE: check mode is DELETE_NODE
+// check mode is DELETE_NODE
 export const isDeleteNodeMode = () => window.EDITOR_MODE === EDITOR_MODE.DELETE_NODE
 
-// NOTE: check mode is DELETE_LINE
+// check mode is DELETE_LINE
 export const isDeleteLineMode = () => window.EDITOR_MODE === EDITOR_MODE.DELETE_LINE
 
-// NOTE: get pass data in global variable
+// get pass data in global variable
 export const getPassData = () => window.PASS_DATA
 
-// NOTE: set pass data to global variable
+// set pass data to global variable
 export const setPassData = data => {
   window.PASS_DATA = data
 }
 
-// NOTE: clear pass data from global variable
+// clear pass data from global variable
 export const clearPassData = () => {
   window.PASS_DATA = null
 }
 
-// NOTE: return pass data before clear pass data
+// return pass data before clear pass data
 export const getPassDataBeforeClear = () => {
   const data = getPassData()
   clearPassData()
   return data
 }
 
-// NOTE: get editor mode to global variable
+// get editor mode to global variable
 export const getEditorMode = () => window.EDITOR_MODE
 
-// NOTE: set editor mode to global variable
+// set editor mode to global variable
 export const setEditorMode = data => {
   window.EDITOR_MODE = data
 
-  // NOTE: show mode after set mode
+  // show mode after set mode
   showCurrentMode(window.EDITOR_MODE)
 }
 
-// NOTE: set data to global variable
+// set data to global variable
 export const setDataToGlobal = (name, value) => {
   window[name] = value
 }
 
-// NOTE: get data from global variable
+// get data from global variable
 export const getDataFromGlobal = name => window[name]
 
-// NOTE: get current mode in global variable
+// get current mode in global variable
 export default EDITOR_MODE
