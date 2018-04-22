444ce10c6176834068f4733f28b745f68b63e8cc
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-22 13:58:27 +0700

    Add load data on app loaded

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,2 +1,8 @@
+import loadMode from './mode/loadMode'
+
 // disable right click contextmenu
 window.addEventListener('contextmenu', event => event.preventDefault())
+
+window.addEventListener('load', () => {
+  loadMode()
+})
