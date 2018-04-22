88248dfd03d840a2e72d77adc1726f6106c6dce7
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-22 08:07:20 +0700

    Add getDiagramSize

--- frontend/src/js/utility/getArea.js
+++ frontend/src/js/utility/getArea.js
@@ -1,11 +1,20 @@
-// NOTE: Get root area group
+// Get root area group
 export const getRootArea = () => d3.select('#root-area-group')
 
-// NOTE: Get temp area group
+// Get temp area group
 export const getTempArea = () => d3.select('#temp-area-group')
 
-// NOTE: Get draw area group
+// Get draw area group
 export const getDrawArea = () => d3.select('#draw-area-group')
 
-// NOTE: get grid area group
+// get grid area group
 export const getGridArea = () => d3.select('#grid-area-group')
+
+export const getDiagramSize = () => {
+  const bbox = d3
+    .select('#diagram-drawing')
+    .node()
+    .getBBox()
+  const { width, height } = bbox
+  return { width, height }
+}
