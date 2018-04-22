d2be0ed6d34a13c4d34399480618144d6954b358
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-22 17:18:05 +0700

    Create mode/importMode.js

---
+++ frontend/src/js/mode/importMode.js
@@ -0,0 +1,29 @@
+import loadMode from './loadMode'
+
+let fileUpload = null
+
+// https://jsfiddle.net/Ln37kqc0/
+window.addEventListener('load', () => {
+  document.getElementById('import').addEventListener('click', () => {
+    fileUpload = document.createElement('input')
+    fileUpload.setAttribute('type', 'file')
+    fileUpload.setAttribute('name', 'import-file')
+
+    fileUpload.addEventListener('change', function() {
+      const { files } = this
+      if (files.length <= 0) return
+      if (files[0].type !== 'application/json') return
+
+      const fr = new FileReader()
+      fr.onload = e => {
+        if (e.target.result.length === 0) return
+
+        const result = JSON.parse(e.target.result)
+        loadMode(result)
+      }
+      fr.readAsText(files[0])
+    })
+
+    fileUpload.click()
+  })
+})
