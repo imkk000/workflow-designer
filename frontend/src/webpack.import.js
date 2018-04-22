70518f8734dcd1069666de966f4ee7b55e63d70d
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-22 13:27:07 +0700

    Add loadMode to webpack import file

--- frontend/src/webpack.import.js
+++ frontend/src/webpack.import.js
@@ -10,6 +10,7 @@ import './js/onload/addNodePreviews'
 import './js/contextmenu'
 import './js/editor/grid'
 import './js/mode/saveMode'
+import './js/mode/loadMode'
 
 // NOTE: Import style(css, scss) app dependencies
 import '../node_modules/roboto-fontface/css/roboto/sass/roboto-fontface.scss'
