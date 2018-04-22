40753cc41cff301f77b72112f64fdba328171800
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-22 18:06:21 +0700

    Add remove all object in canvas

--- frontend/src/js/onload/initialGlobalVariable.js
+++ frontend/src/js/onload/initialGlobalVariable.js
@@ -1,11 +1,22 @@
 import GraphDataStructure from 'graph-data-structure'
 import { setDataToGlobal } from '../utility/editorMode'
+import { getDrawArea } from '../utility/getArea'
 
-// initial global variable project
-window.addEventListener('load', () => {
+const initialGlobalVariable = () => {
+  getDrawArea()
+    .selectAll('*')
+    .remove()
+
   setDataToGlobal('EDITOR_MODE', 'NORMAL')
   setDataToGlobal('NODES_PROPERTIES', {})
   setDataToGlobal('NODES', {})
   setDataToGlobal('LINES', {})
   setDataToGlobal('GRAPH', new GraphDataStructure())
+}
+
+// initial global variable project
+window.addEventListener('load', () => {
+  initialGlobalVariable()
 })
+
+export default initialGlobalVariable
