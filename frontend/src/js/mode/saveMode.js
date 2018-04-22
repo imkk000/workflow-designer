21edbf46a972f9934f9891585431183ccc69d446
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-22 13:58:58 +0700

    Fix filter value before save

--- frontend/src/js/mode/saveMode.js
+++ frontend/src/js/mode/saveMode.js
@@ -1,11 +1,39 @@
 import { getDataFromGlobal } from '../utility/editorMode'
 
+const saveMode = () => {
+  const nodes = getDataFromGlobal('NODES')
+  const lines = getDataFromGlobal('LINES')
+
+  const newNodes = Object.values(nodes).map(node => {
+    const { settings } = node
+
+    // remove value depth 0
+    delete node.label
+    delete node.limitInput
+
+    // remove value depth 1: settings
+    Object.keys(settings).map(settingKey => {
+      const { value } = settings[settingKey]
+      settings[settingKey] = { value }
+      return true
+    })
+
+    return node
+  })
+
+  const newLines = Object.keys(lines).map(lineId => ({
+    lineId,
+    ...lines[lineId],
+  }))
+
+  localStorage.setItem('NODES', JSON.stringify(newNodes))
+  localStorage.setItem('LINES', JSON.stringify(newLines))
+}
+
 window.addEventListener('load', () => {
   document.getElementById('save').addEventListener('click', () => {
-    const nodes = getDataFromGlobal('NODES')
-    const lines = getDataFromGlobal('LINES')
-
-    localStorage.setItem('NODES', JSON.stringify(nodes))
-    localStorage.setItem('LINES', JSON.stringify(lines))
+    saveMode()
   })
 })
+
+export default saveMode
