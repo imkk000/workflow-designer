02687d29f5ada14b888b96f8d64017f316cce865
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-23 15:14:33 +0700

    Add server api

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,3 +2,27 @@ import cv from 'opencv4nodejs'
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+
+const PORT = process.env.PORT || 1412
+const app = express()
+const router = express.Router()
+
+app
+  .use(cors())
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(bodyParser.json())
+  .use('/api', router)
+  .listen(PORT)
+
+router
+  .route('/process')
+  .get(async (req, res) => {
+    try {
+      const img = cv.imreadAsync('')
+    } catch (err) {
+      console.error(err)
+    }
+
+    console.log('fix!')
+    res.json({ null: null })
+  })
