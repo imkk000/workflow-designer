aaa9c8697b12e5aebce4d7c14967e29a617a3715
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-23 08:35:56 +0700

    Move app.js to src

--- backend/app.js
+++
@@ -1,3 +0,0 @@
-const cv = require('opencv4nodejs')
-const mat = new cv.Mat(100, 100, cv.CV_8UC1, 255)
-console.log(mat)
---
+++ backend/src/app.js
@@ -0,0 +1,4 @@
+import cv from 'opencv4nodejs'
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
