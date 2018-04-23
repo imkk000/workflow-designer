c55ba43daa5e7703ea11a90f3107d7285b269660
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-23 18:59:11 +0700

    Add upload api

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,8 @@ import functions from './functions'
 import fs from 'fs'
 import path from 'path'
 import rimraf from 'rimraf'
+import fileUpload from 'express-fileupload'
+import { uploadFile } from './functions/imagePath'
 
 const PORT = process.env.PORT || 1412
 const app = express()
@@ -15,24 +17,35 @@ app
   .use(cors())
   .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
+  .use(fileUpload())
   .use('/api', router)
   .listen(PORT)
 
 router
   .route('/process')
-  .get(async (req, res) => {
+  .post((req, res) => {
     const { data } = req.body
     res.json({ null: null })
   })
 
-const data = {
-  files: [
-    {
-      fileId: 'tux_profile',
-      fileExt: 'png',
-    },
-  ]
-}
+router
+  .route('/upload')
+  .post((req, res) => {
+    try {
+      if (!req.files) return res.status(400)
+
+      const { name, mimetype, md5, mv } = req.files.file
+      const fileExt = mimetype.match(/(png|jpg|jpeg)/g)[0]
+      const fileName = uploadFile({ fileId: md5, fileExt })
+
+      if (!fs.existsSync(fileName)) mv(fileName)
+
+      res.json({ fileId: md5 })
+    } catch (err) {
+      res.status(400)
+    }
+  })
+
 const oneData = {
   type: 'LoadImageFunction',
   files: {
