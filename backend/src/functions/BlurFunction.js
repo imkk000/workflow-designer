db6cc3c3376da41f7c22bfff29eaa915eeff6ef3
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-23 21:31:33 +0700

    Create functions/BlurFunction.js

---
+++ backend/src/functions/BlurFunction.js
@@ -0,0 +1,19 @@
+import cv from 'opencv4nodejs'
+import path from 'path'
+import { sayFileName, saveFile } from './imagePath'
+
+export default (data) => {
+  return new Promise((resolve, reject) => {
+    const fileName = sayFileName(data)
+    const img = cv.imread(fileName)
+
+    const { settings } = data
+    const { sigma } = settings
+
+    const size = new cv.Size()
+    const gaussian = img.gaussianBlur(size, sigma)
+    console.log(gaussian)
+
+    resolve(saveFile(gaussian))
+  })
+}
--- backend/src/functions/index.js
+++ backend/src/functions/index.js
@@ -1,8 +1,10 @@
 import LoadImageFunction from './LoadImageFunction'
 import RotateFunction from './RotateFunction'
+import BlurFunction from './BlurFunction'
 
 // function registry
 export default {
   LoadImageFunction,
   RotateFunction,
+  BlurFunction,
 }
