c710e47188ed917c798dda5ffa2bc497cc24b7a4
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-23 17:21:16 +0700

    Create functions/index.js

---
+++ backend/src/functions/index.js
@@ -0,0 +1,8 @@
+import LoadImageFunction from './LoadImageFunction'
+import RotateFunction from './RotateFunction'
+
+// function registry
+export default {
+  LoadImageFunction,
+  RotateFunction,
+}
