2b3b54d63e1b1da4e34d06dd0d933f0ba551773d
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-23 21:46:17 +0700

    Add new functions

--- backend/src/functions/index.js
+++ backend/src/functions/index.js
@@ -1,10 +1,14 @@
 import LoadImageFunction from './LoadImageFunction'
 import RotateFunction from './RotateFunction'
 import BlurFunction from './BlurFunction'
+import ConvertGrayscaleFunction from './ConvertGrayscaleFunction'
+import CannyFunction from './CannyFunction'
 
 // function registry
 export default {
   LoadImageFunction,
   RotateFunction,
   BlurFunction,
+  ConvertGrayscaleFunction,
+  CannyFunction,
 }
