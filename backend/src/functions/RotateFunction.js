c9b7ad4cc1eeee7a292fbf117029c2cd4dfdda4a
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-23 18:58:03 +0700

    Fix RotateFunction

--- backend/src/functions/RotateFunction.js
+++ backend/src/functions/RotateFunction.js
@@ -9,22 +9,24 @@ export default (data) => {
   const { settings } = data
   const { angle } = settings
 
-  const { rows, cols } = img
-  const [centerX, centerY] = [rows / 2, cols / 2]
+  // fix rows, cols
+  const [height, width] = img.sizes
+  const centerX = width / 2
+  const centerY = height / 2
   const centerPoint = new cv.Point2(centerX, centerY)
-  const rotMat2D = cv.getRotationMatrix2D(centerPoint, angle, 1.0)
+  const rotMat2D = cv.getRotationMatrix2D(centerPoint, -angle, 1.0)
   const cos = Math.abs(rotMat2D.at(0, 0))
   const sin = Math.abs(rotMat2D.at(0, 1))
-  const newW = Math.floor((cols * sin) + (rows * cos))
-  const newH = Math.floor((cols * cos) + (rows * sin))
-  const newSize = new cv.Size(newW, newH)
+  const newWidth = Math.floor((height * sin) + (width * cos))
+  const newHeight = Math.floor((height * cos) + (width * sin))
+  const newSize = new cv.Size(newWidth, newHeight)
 
-  rotMat2D.set(0, 2, rotMat2D.at(0, 2) + (newW / 2) - centerX)
-  rotMat2D.set(1, 2, rotMat2D.at(1, 2) + (newH / 2) - centerY)
+  rotMat2D.set(0, 2, rotMat2D.at(0, 2) + (newWidth / 2) - centerX)
+  rotMat2D.set(1, 2, rotMat2D.at(1, 2) + (newHeight / 2) - centerY)
 
   const newMat = img.warpAffine(rotMat2D, newSize)
   const srcImg = newMat
-  const tmpImg = new cv.Mat(rows, cols, cv.CV_8UC4, [255, 255, 255, 255])
+  const tmpImg = new cv.Mat(width, height, cv.CV_8UC4, [255, 255, 255, 255])
     .cvtColor(cv.COLOR_BGR2GRAY)
     .warpAffine(rotMat2D, newSize)
 
