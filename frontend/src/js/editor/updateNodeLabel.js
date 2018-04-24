d022779ff07244e7a765544259efcfd360bf71ed
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-24 08:47:32 +0700

    Fix generate node label size

--- frontend/src/js/editor/updateNodeLabel.js
+++ frontend/src/js/editor/updateNodeLabel.js
@@ -3,18 +3,19 @@ import { getDataFromGlobal } from '../utility/editorMode'
 export default nodeId => {
   const nodes = getDataFromGlobal('NODES')
   const graph = getDataFromGlobal('GRAPH')
-  const node = d3.select($(`#${nodeId}`).get(0))
+  const node = d3.select(document.getElementById(nodeId))
   const nodeInDegree = graph.indegree(nodeId)
   const { label, limitInput } = nodes[nodeId]
   const nodeCountLabel = `${nodeInDegree}/`
   const newLabel = `(${nodeInDegree > 0 ? nodeCountLabel : ''}${limitInput}) ${label}`
-  const nodeLabel = node.select('.node-label').text(newLabel.toUpperCase())
+  const nodeLabel = node.select('.node-label').text(newLabel)
   const textBBox = nodeLabel.node().getBBox()
   const { width: textWidth, height: textHeight } = textBBox
-  const [rectWidthOffset, rectHeightOffset] = [15, 15]
+  const newWidth = Math.max(120, textWidth)
+  const newHeight = Math.max(35, textHeight)
 
   node
     .select('.node-box')
-    .attr('width', textWidth + rectWidthOffset)
-    .attr('height', textHeight + rectHeightOffset)
+    .attr('width', newWidth)
+    .attr('height', newHeight)
 }
