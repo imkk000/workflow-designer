7ed335699e4af390039667fb4082a7859db5059e
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-24 13:01:55 +0700

    Fix node label pattern

--- frontend/src/js/editor/updateNodeLabel.js
+++ frontend/src/js/editor/updateNodeLabel.js
@@ -5,16 +5,33 @@ export default nodeId => {
   const graph = getDataFromGlobal('GRAPH')
   const node = d3.select(document.getElementById(nodeId))
   const nodeInDegree = graph.indegree(nodeId)
-  const { label, limitInput } = nodes[nodeId]
-  const nodeCountLabel = `${nodeInDegree}/`
-  const newLabel = `(${nodeInDegree > 0 ? nodeCountLabel : ''}${limitInput}) ${label}`
+  const nodeOutDegree = graph.outdegree(nodeId)
+  const { label, limitInput, settings } = nodes[nodeId]
+
+  const settingsArray = Object.keys(settings)
+  let paramsString = ''
+  if (settingsArray.length) {
+    settingsArray.map((key, index) => {
+      paramsString += `${settings[key].value}`
+      if (settingsArray.length - 1 > index) paramsString += ', '
+      return true
+    })
+    paramsString = `(${paramsString}) `
+  }
+
+  const newLabel = `${limitInput}-${label} ${paramsString} (${nodeInDegree}, ${nodeOutDegree})`
   const nodeLabel = node.select('.node-label').text(newLabel)
   const textBBox = nodeLabel.node().getBBox()
   const { width: textWidth, height: textHeight } = textBBox
-  const newWidth = Math.max(105, textWidth)
+  const newWidth = Math.max(90, textWidth) + 15
   const newHeight = Math.max(35, textHeight)
+
+  // fix node label center
+  nodeLabel.attr('x', newWidth / 2)
+  nodeLabel.attr('y', newHeight / 2)
+
   node
     .select('.node-box')
-    .attr('width', newWidth + 15)
+    .attr('width', newWidth)
     .attr('height', newHeight)
 }
